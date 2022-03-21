397d7cada140808a6b7734a67804fd156556b63c
Haroune Hdd <75623924+HarouneHdd@users.noreply.github.com>
2022-03-21 22:10:13 +0100

    Fix renderAction() on Track component

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,9 +2,14 @@ import React from "react";
 import './Track.css';
 
 export class Track extends React.Component {
-    renderAction(button) {
-        const btnText = this.isRemoval ? '-' : '+';
-        button.innerHTML = btnText;
+    renderAction() {
+        if (this.props.isRemoval) 
+        {
+            return <button className="Track-action">-</button>;
+        }
+        else {
+            return <button className="Track-action">+</button>;
+        }
     }
     
     render() {
